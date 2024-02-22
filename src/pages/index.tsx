fb742ba0ce1e30a189530c3ca3fa8a41cb37d4ca
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-22 09:15:11 -0500

    Rearrange images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Image from 'next/image';
 import portrait from '../../public/images/ks-portrait.jpeg';
+import flowers from '../../public/images/ks-flowers.jpg';
 const Home = () => {
   return (
     <main className='fade-in'>
-      <Image className='opacity-60 filter blur-xs grayscale-50 w-screen object-cover h-[500px] lg:h-[600px]' src={portrait} alt='katie sanger portrait' />
+      <Image className='opacity-60 filter blur-xs grayscale-50 w-screen h-nav-vh object-cover' src={portrait} alt='katie sanger portrait' />
     </main>
   );
 };
