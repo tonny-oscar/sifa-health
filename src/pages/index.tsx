86f565e0bd77db97c45bea7b943971f9dc3b5f19
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-23 15:13:58 -0500

    Add mini about section to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Layout from '@/components/Layout';
 import moonphase from '../../public/images/moonphase.png';
 import moonsun from '../../public/images/moon-sun.png';
+import Link from 'next/link';
 
 const Home = () => {
   return (
@@ -21,6 +22,15 @@ const Home = () => {
         <Image src={moonsun} alt='moon and sun decorative image' className='max-w-screen w-[150px]' />
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div id='homeAboutSection' className='min-h-[420px] flex flex-col justify-center md:grid grid-cols-2'>
+        <h1 className='self-center text-center text-4xl font-semibold m-2'>Aligning Mind, Stars, &amp; Spirit</h1>
+        <div className='bg-white bg-opacity-90 m-2 rounded-sm p-4 h-auto flex flex-col justify-around'>
+          <p className='m-1'>Step into a holistic journey of self-discovery with Katie, where life coaching, astrology, and the serene wisdom of yoga converge. As a compassionate guide, Katie seamlessly blends her expertise as a life coach with celestial insights, providing a unique and transformative experience. Rooted in her yogic practice, Katie fosters mindfulness and presence in every interaction. Her nurturing approach gently empowers individuals to navigate life's complexities, unravel their true potential</p>
+          <Link href='/about' className='m-1 self-center border-[1px] border-black py-2 px-5'>
+            MORE ABOUT ME
+          </Link>
+        </div>
       </div>
     </Layout>
   );
