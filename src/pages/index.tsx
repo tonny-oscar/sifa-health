2a432a19b279b541310be3cb784733562db07926
Laura Guerra <l.garciaguerra1@gmail.com>
2024-02-26 21:06:54 -0500

    Switch to articles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,15 +17,15 @@ const Home = () => {
           <Image src={moonphase} alt='moonphases' className='max-w-screen w-[250px]' />
         </article>
       </div>
-      <div className='min-h-[400px] h-auto border-t-[2px] border-linen bg-white p-5 text-center flex flex-col items-center justify-around'>
+      <article className='min-h-[400px] h-auto border-t-[2px] border-linen bg-white p-5 text-center flex flex-col items-center justify-around'>
         <h1 className='text-xl'>How do coaching and astrology blend?</h1>
         <Image src={moonsun} alt='moon and sun decorative image' className='max-w-screen w-[150px]' />
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
         <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div id='homeAboutSection' className='min-h-[420px] flex flex-col justify-center md:grid grid-cols-2'>
+      </article>
+      <article id='homeAboutSection' className='min-h-[420px] flex flex-col justify-center md:grid grid-cols-2'>
         <div className='bg-white bg-opacity-60 mt-4 md:mt-0 md:bg-transparent rounded-sm p-4 h-auto flex flex-col justify-around'>
-          <h1 className='self-center text-center text-3xl font-semibold m-2'>Aligning Mind, Stars, &amp; Spirit</h1>
+          <h1 className='self-center text-center text-3xl font-regular md:font-semibold m-2'>Aligning Mind, Stars, &amp; Spirit</h1>
         </div>
         <div className='bg-white bg-opacity-90 mb-4 md:m-2 rounded-sm p-4 h-auto flex flex-col items-center justify-around'>
           <p className='m-1 max-w-[800px]'>Step into a holistic journey of self-discovery with Katie, where life coaching, astrology, and the serene wisdom of yoga converge. As a compassionate guide, Katie seamlessly blends her expertise as a life coach with celestial insights, providing a unique and transformative experience. Rooted in her yogic practice, Katie fosters mindfulness and presence in every interaction. Her nurturing approach gently empowers individuals to navigate life&apos;s complexities, unravel their true potential</p>
@@ -33,7 +33,20 @@ const Home = () => {
             MORE ABOUT ME
           </Link>
         </div>
-      </div>
+      </article>
+      <section id='homeServicesSection' className='min-h-[400px] flex flex-col items-center justify-around p-5'>
+        <h1 className='text-3xl text-center m-2 '>WHAT I OFFER</h1>
+
+        <article className='min-h-[250px] m-5 bg-white bg-opacity-50 rounded-sm flex flex-col items-center justify-center text-center'>
+          <h2>Coaching Services</h2>
+          <p className='max-w-[400px] md:max-w-[70vw] m-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </article>
+        <button>Arrow</button>
+
+        <Link className='border-[1px] border-black py-2 px-5' href='/services'>
+          LEARN MORE
+        </Link>
+      </section>
     </Layout>
   );
 };
