d720582a6e7bd762c29c299f2e1ede2ef03a20db
Laura Guerra <l.garciaguerra1@gmail.com>
2024-03-12 12:36:19 -0400

    Add service descriptions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,8 @@ const Home = () => {
       <article className='min-h-[400px] h-auto border-t-[2px] border-linen bg-white p-5 text-center flex flex-col items-center justify-around'>
         <h1 className='text-xl'>How do coaching and astrology blend?</h1>
         <Image src={moonsun} alt='moon and sun decorative image' className='max-w-screen w-[150px]' />
-        <p>In my coaching practice, I seamlessly integrate astrology as a transformative tool, using it as a guiding map for our collaborative journey. Astrology, with its profound insights derived from the birth chart, becomes the foundation for understanding and addressing my clients' unique needs, strengths, desires, challenges, and values. This celestial framework serves as a powerful guide for setting personalized goals and taking aligned actions, especially in the realms of relationships, career, and self-healing.</p>
-        <p className='my-4'>By combining mindset work, somatics, and embodiment practices with astrology, I create a holistic approach that goes beyond traditional coaching, offering a rich, tailored therapeutic experience. Embracing the interplay of astrology and coaching allows us to unlock the depths of the psyche, facilitating profound personal growth and alignment with one's authentic self.</p>
+        <p>In my coaching practice, I seamlessly integrate astrology as a transformative tool, using it as a guiding map for our collaborative journey. Astrology, with its profound insights derived from the birth chart, becomes the foundation for understanding and addressing my clients&apos; unique needs, strengths, desires, challenges, and values. This celestial framework serves as a powerful guide for setting personalized goals and taking aligned actions, especially in the realms of relationships, career, and self-healing.</p>
+        <p className='my-4'>By combining mindset work, somatics, and embodiment practices with astrology, I create a holistic approach that goes beyond traditional coaching, offering a rich, tailored therapeutic experience. Embracing the interplay of astrology and coaching allows us to unlock the depths of the psyche, facilitating profound personal growth and alignment with one&apos;s authentic self.</p>
       </article>
       <article id='homeAboutSection' className='min-h-[420px] flex flex-col justify-center md:grid grid-cols-2'>
         <div className='bg-white bg-opacity-60 mt-4 md:mt-0 md:bg-transparent rounded-sm p-4 h-auto flex flex-col justify-around'>
@@ -37,34 +37,48 @@ const Home = () => {
       </article>
       <section id='homeServicesSection' className='min-h-[400px] flex flex-col items-center justify-around p-5'>
         <h1 className='text-3xl text-center m-2 '>WHAT I OFFER</h1>
-
-        <article className='min-h-[250px] m-5 bg-white bg-opacity-50 rounded-sm flex flex-col items-center justify-center text-center'>
+        <article className='w-[90vw] md:max-w-[900px] min-h-[250px] m-5 bg-white bg-opacity-50 rounded-sm flex flex-col items-center justify-center text-center'>
           {coachingShown ? (
             <>
-              <h2>Coaching Services</h2>
-              <p className='max-w-[400px] md:max-w-[70vw] m-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <h2 className='text-xl border-b-2 text-center'>Coaching Services</h2>
+              <div className='max-w-[400px] md:max-w-[70%] mx-5'></div>
+              <ul className='my-4 mx-8 text-start'>
+                <li className='p-1'>
+                  <span className='font-bold text-lg'>3 Session Catalyst:</span> DESCRIPTION NEEDED HERE
+                </li>
+                <li className='p-1'>
+                  <span className='font-bold text-lg'>3 Month Program:</span> DESCRIPTION NEEDED HERE
+                </li>
+                <li className='p-1'>
+                  <span className='font-bold text-lg'>6 Month Program:</span> DESCRIPTION NEEDED HERE
+                </li>
+              </ul>
             </>
           ) : (
             <>
-              <h2>Astrology Services</h2>
-              <p className='max-w-[400px] md:max-w-[70vw] m-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+              <h2 className='text-xl border-b-2 text-center'>Astrology Offerings</h2>
+              <div className='max-w-[400px] md:max-w-[70%] mx-5'></div>
+              <ul className='my-4 mx-8 text-start'>
+                <li className='p-1'>
+                  <span className='font-bold text-lg'>Basic Reading:</span> You will receive a PDF and a 20 minute recording.
+                </li>
+                <li className='p-1'>
+                  <span className='font-bold text-lg'>In Depth Astrology Session:</span> 1 hour long live session.
+                </li>
+                <li className='p-1'>
+                  <span className='font-bold text-lg'>Power Moon Reading:</span> Comparing your birth chart to the themes of the current new or full moon.
+                </li>
+              </ul>
             </>
           )}
+          <div>
+            <button className={`m-1 rounded-full h-4 w-4 ${coachingShown ? 'bg-gray-400' : 'bg-none'} border-2`} onClick={() => setCoachingShown(true)}></button>
+            <button className={`m-1 rounded-full h-4 w-4 ${!coachingShown ? 'bg-gray-400' : 'bg-none'} border-2`} onClick={() => setCoachingShown(false)}></button>
+          </div>
         </article>
-        <button onClick={() => setCoachingShown((prev) => !prev)}>
-          {coachingShown ? (
-            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3' />
-            </svg>
-          ) : (
-            <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6'>
-              <path strokeLinecap='round' strokeLinejoin='round' d='M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18' />
-            </svg>
-          )}
-        </button>
 
-        <Link className='border-[1px] border-black py-2 px-5' href='/services'>
-          LEARN MORE
+        <Link className='border-b-[1px] border-black py-2 px-5' href='#'>
+          BOOK A FREE CLARITY CALL
         </Link>
       </section>
     </Layout>
