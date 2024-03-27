e49144a1d08e5c0ea6ea32c27e8395aa6b06727c
Laura Guerra <l.garciaguerra1@gmail.com>
2024-03-26 20:41:01 -0400

    Update prices and fix typos

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,17 +2,17 @@ export const astrologyServices = [
     {
       name: 'Big Six Reading',
       description: 'A thirty-minute recording sent to you via e-mail. Intuitive downloads and insights focusing solely on the interpersonal planets in your birth chart: Sun, Moon, Rising, Mercury, Venus and Mars. Tarot/Angel Cards included. Inquiry form and invoice will be sent to you upon booking.',
-      price: '$88',
+      price: '$66',
     },
     {
       name: 'Intuitive Birth Chart Reading',
       description: 'A one-hour recording sent to you via e-mail. Intuitive downloads and insights going in depth into your birth chart, discussing unique placements, aspects, specific transits you may be currently undergoing and anything else that stands out to me. Tarot/Angel Cards included. Inquiry form and invoice will be sent to you upon booking.',
-      price: '$166',
+      price: '$122',
     },
     {
       name: 'Power Moon Reading',
       description: 'A thirty-minute recording sent to you via e-mail. Interpreting and discussing your unique birth chart and releveant themes, utilizing the current upcoming moon transit (either full moon or new moon). Inquiry form and invoice will be sent to you upon booking.',
-      price: '$88',
+      price: '$66',
     },
 ];
   
@@ -20,16 +20,16 @@ export const coachingServices = [
     {
       name: 'Three Session Catalyst',
       description: 'A great way to preview the impactful nature of our work together. Three 60 minute coaching sessions, utilizing the astrological birth chart and the Ennegram as maps for our sessions. Goal and intention setting, mindset work, strategizing, and setting you up with a go-forward strategy. Begin by booking clarity call.',
-      price: 'Quoted during during clarity call',
+      price: 'Quoted during clarity call',
     },
     {
       name: 'Three Month Container',
       description: 'In this type of container, we can dive more deeply into our work together, resulting in profound transformation. Do not be surpirsed if by the end of this, your life starts to look and feel wildly different (in the best of ways). Requires a commitment and dedication to making the lasting changes you seek. ',
-      price: 'Quoted during during clarity call',
+      price: 'Quoted during clarity call',
     },
     {
       name: 'Private Yoga Therapy',
       description: 'One hour private session utilizing yoga and therapeutic coaching as the basis to heal the mind and body in an integrative way. Hatha yoga based, utilizing breathwork, yin, and yoga psychology principles. Walk away feeling integrated and refreshed in both mind and body. Begin by booking a clarity call',
-      price: 'Quoted during during clarity call',
+      price: 'Quoted during clarity call',
     },
   ];
