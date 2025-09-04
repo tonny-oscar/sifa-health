d68e96da0f35ea8a50cf4ed3464c590f2fbcce16
user-pc <betttonny26@gmail.com>
2025-09-04 17:27:30 +0300

    logo from footer change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '../../public/images/logo.png'; 
+import logo from '../../public/images/logo-sifa.png'; 
 import minimoon from '../../public/images/minimoon.png';
 
 const Footer = () => {
